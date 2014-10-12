43d965847d12699edcaf1631fd59f26a85127438
Ally Hinton <allyhinton383@gmail.com>
2014-10-12 23:02:36 +0000

    adding my neighbots & their positions.

---
+++ 08-Conway-Xx-Nova-Xx.js
@@ -0,0 +1,9 @@
+//Let's Build Conway & take over the world, one cube at a time. 
+
+var board = [
+    ["false","false","false"],
+    ["false","false","false"], 
+    ["false","false","false"], 
+    ];
+    
+    
