a261a59acfb2c5ecd23ed9210f332cde1ed65b79
Ally Hinton <allyhinton383@gmail.com>
2014-10-13 01:52:31 +0000

    Got a conway, failing but there.

--- 08-Conway-Xx-Nova-Xx.js
+++ 08-Conway-Xx-Nova-Xx.js
@@ -95,7 +95,7 @@ var board = [
 
 ///Testing 1, 2, 3...
 
-        console.log("Got here!");
+      
 
     /*    var assert = require("assert");
     
@@ -121,12 +121,13 @@ var board = [
         
 */
 
-var assert = require('assert');
+/*var assert = require('assert');
 
+  console.log("Got here!");
 
 describe('neighbotsLive', function(){
   it('should return an array of neighbor values given a cell', function(){
-    assert.Equal(neighbotsLive(board[0][0]), ([board[0][1], board[1][0], board[1][1]]));
+    assert.Equal(neighbotsLive(board, 0, 0), ([board[0][1], board[1][0], board[1][1]]));
   });
 });
 
@@ -178,4 +179,51 @@ var board7   = [
                 ]
                 
 
+*/
 
+//Tests from elsewhere
+var expect = require('chai').expect;
+var should = require('chai').should();
+var assert = require('chai').assert;
+
+/**
+ * Log `success` if `actual` is STRICTLY equal to `expected`
+ *
+ * @param ANY actual
+ * @param ANY expected
+ * @param String success
+ */
+function test(actual, expected, success){
+    if (success === undefined) success = 'Wait Really? It worked? Holy Shit it WORKED!!!';
+    assert.strictEqual(actual, expected);
+    console.log(success);
+}
+
+
+  describe("neighborsOf(), which assigns the neighbors of a cell given a board and a cells specific coordinates", function(){
+    it ('should return 3 neighbors for 0,0', function(){
+      assert.deepEqual(neighborsLive(board, 0, 0), [false,false,false]);
+    })
+    it ('"EXPECT" to be an Array for 0,0', function(){
+      expect(neighborsLive(board,0,0)).to.be.an('Array');
+    })
+    it ('should return 5 neighbors for 0,1', function(){
+      assert.deepEqual(neighborsLive(board, 0, 1), [false,false,false,false,false]);
+    })
+    it ('"EXPECT" to be an Array for 0,1', function(){
+      expect(neighborsLive(board,0,1)).to.be.an('Array');
+    })
+    it ('should return 8 neighbors for 1,1', function(){
+      assert.deepEqual(neighborsLive(board, 1, 1), [false,false,false,false,false,false,false,false]);
+    })
+    it ('"SHOULD" be an Array for 1,1', function(){
+      neighborsLive(board,1,1).should.be.an('Array');
+    })
+    it ('should return that neighborsOf is a Function', function(){
+      assert.isFunction(neighborsLive);//chai testing
+    })
+    it ('should "expect" neighbors to not equal true', function(){
+      expect(neighborsLive).to.not.equal(true);
+    })
+
+  })
