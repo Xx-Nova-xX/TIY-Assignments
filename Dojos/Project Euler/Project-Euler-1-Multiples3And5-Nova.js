326d4d04de13305ea3d38beccc723e4d8aa46b8c
Ally Hinton <allyhinton383@gmail.com>
2014-10-15 05:19:54 +0000

    Project Euler 1 adding code and comments

--- Dojos/Project Euler/Project-Euler-1-Multiples3And5-Nova.js
+++ Dojos/Project Euler/Project-Euler-1-Multiples3And5-Nova.js
@@ -1,10 +1,17 @@
 /*Problem 1
     If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
-    The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 10
+    The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000
 */
 
 
-var sum = 0
-var multiples
+var sum = 0; //variable for the sum of all the numbers 
 
-if 
+    for (var multipleMaybe = 0; multipleMaybe < 1000; multipleMaybe++) { //multipleMaybe is numbers 1-999 to be passed through 
+                                                                        //it starts at 0, checks to see if it is 0-999 and then adds one 
+        if(multipleMaybe % 3 === 0 || multipleMaybe %  5 === 0) { //if multipleMaybe is divided by 3 or 5 with a remainder of 0 send to 
+        sum += multipleMaybe;            //this is like saying sum = multipleMaybe + multipleMaybe each pass
+        }
+    }
+        console.log(sum); //logs the final total of the multiples
+        
+        
