565811834f67dfccef866480c7425185e4ecf0bf
Ally Hinton <allyhinton383@gmail.com>
2014-10-16 21:42:42 +0000

    1st Euler OOP refactor start

--- Dojos/Project Euler/Project-Euler-1-Multiples3And5-Nova.js
+++ Dojos/Project Euler/Project-Euler-1-Multiples3And5-Nova.js
@@ -3,15 +3,20 @@
     The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000
 */
 
-
-var sum = 0; //variable for the sum of all the numbers 
-
-    for (var multipleMaybe = 0; multipleMaybe < 1000; multipleMaybe++) { //multipleMaybe is numbers 1-999 to be passed through 
-                                                                        //it starts at 0, checks to see if it is 0-999 and then adds one 
-        if(multipleMaybe % 3 === 0 || multipleMaybe %  5 === 0) { //if multipleMaybe is divided by 3 or 5 with a remainder of 0 send to 
-        sum += multipleMaybe;            //this is like saying sum = multipleMaybe + multipleMaybe each pass
-        }
-    }
-        console.log(sum); //logs the final total of the multiples
         
+ function solution(){
+     return {
+         solveFor: function(){
+            
+            var sum = 0; //variable for the sum of all the numbers 
         
+            for (var multipleMaybe = 0; multipleMaybe < 1000; multipleMaybe++) { //multipleMaybe is numbers 1-999 to be passed through 
+                                                                                //it starts at 0, checks to see if it is 0-999 and then adds one 
+                if(multipleMaybe % 3 === 0 || multipleMaybe %  5 === 0) { //if multipleMaybe is divided by 3 or 5 with a remainder of 0 send to 
+                sum += multipleMaybe;            //this is like saying sum = multipleMaybe + multipleMaybe each pass
+                }
+            }
+            return sum; 
+         }
+     }
+}       
