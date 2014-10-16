292ea2f843538384acd3bf0fd4961e9e7103b9ca
Ally Hinton <allyhinton383@gmail.com>
2014-10-16 21:48:42 +0000

    Pair programming with PhillyCC, finally finished with Euler 1

--- Dojos/Project Euler/Project-Euler-1-Multiples3And5-Nova.js
+++ Dojos/Project Euler/Project-Euler-1-Multiples3And5-Nova.js
@@ -3,14 +3,15 @@
     The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000
 */
 
-        
+var assert = require('chai').assert;
+
  function solution(){
      return {
-         solveFor: function(){
+         solveFor: function(limit){
             
             var sum = 0; //variable for the sum of all the numbers 
         
-            for (var multipleMaybe = 0; multipleMaybe < 1000; multipleMaybe++) { //multipleMaybe is numbers 1-999 to be passed through 
+            for (var multipleMaybe = 0; multipleMaybe < limit; multipleMaybe++) { //multipleMaybe is numbers 1-999 to be passed through 
                                                                                 //it starts at 0, checks to see if it is 0-999 and then adds one 
                 if(multipleMaybe % 3 === 0 || multipleMaybe %  5 === 0) { //if multipleMaybe is divided by 3 or 5 with a remainder of 0 send to 
                 sum += multipleMaybe;            //this is like saying sum = multipleMaybe + multipleMaybe each pass
@@ -20,3 +21,14 @@
          }
      }
 }       
+
+     describe('solveFor', function() {
+       it('should be a function', function() {
+           assert.isFunction(solution().solveFor, 'function');
+           });
+       it('should be able to solve a simple example', function(){
+          assert.equal(solution().solveFor(10), 23);
+       });
+       
+       
+    });
